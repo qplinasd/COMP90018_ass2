af74391760bf49d0663266f41ca54b1b9f9d8ec1
HAOTIANHE <44996499+HAOTIANHE@users.noreply.github.com>
2021-11-01 16:33:15 +0800

    add detail article

--- app/src/main/java/com/example/recommend/DetailActivity.java
+++ app/src/main/java/com/example/recommend/DetailActivity.java
@@ -1,17 +1,25 @@
 package com.example.recommend;
 
+import static android.service.controls.ControlsProviderService.TAG;
+
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -26,6 +34,13 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -44,21 +59,59 @@ public class DetailActivity extends AppCompatActivity {
     private String name;
     private String password;
     private String databasePassword;
+    private ScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        TextView lblTitle=(TextView)findViewById(R.id.article_title);
+        lblTitle.setText("This is the title");
+        TextView lbArticle = (TextView)findViewById(R.id.article_main);
+        lbArticle.setText("ddddddddddddddddddddddddddddddddddddddddddddddddddddd\n" +
+                "dddddddd");
         initView();
     }
 
     private void initView() {
-
-
+        String imgURL = "";
+        //ImageView img = (ImageView) findViewById(R.id.set_img);
+        //Bitmap bitmap = getLocalBitmap("../../../../../res/drawable/country_test_background2.jpeg");
+        //img.setImageBitmap(bitmap);
 
     }
 
+    public static Bitmap getLocalBitmap(String url) {
+        try {
+            FileInputStream fis = new FileInputStream(url);
+            return BitmapFactory.decodeStream(fis);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
-
+    public static Bitmap getHttpBitmap(String url) {
+        URL myFileUrl = null;
+        Bitmap bitmap = null;
+        try {
+            Log.d(TAG, url);
+            myFileUrl = new URL(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        try {
+            HttpURLConnection conn = (HttpURLConnection) myFileUrl.openConnection();
+            conn.setConnectTimeout(0);
+            conn.setDoInput(true);
+            conn.connect();
+            InputStream is = conn.getInputStream();
+            bitmap = BitmapFactory.decodeStream(is);
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
 
 }
