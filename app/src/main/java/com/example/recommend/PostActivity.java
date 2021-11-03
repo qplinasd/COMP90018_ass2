09b35d8d4e9b4b1b5dbec410dc1babd90795050f
xinhao1 <xinhao1@student.unimelb.edu.au>
2021-11-03 12:06:49 +0800

    (BugFix) save and restore data

--- app/src/main/java/com/example/recommend/PostActivity.java
+++ app/src/main/java/com/example/recommend/PostActivity.java
@@ -1,11 +1,19 @@
 package com.example.recommend;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.TextView;
 
 public class PostActivity extends AppCompatActivity {
+
+    private TextView title;
+    private TextView content;
+    private String TITLE_KEY = "title_key";
+    private String CONTENT_KEY = "content_key";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +22,26 @@ public class PostActivity extends AppCompatActivity {
 
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.nav_host_fragment_content_main, new PostFragment()).commit();
+
+        title = findViewById(R.id.text_post_title);
+        content = findViewById(R.id.text_post_content);
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        String title_str = title.getText().toString();
+        String content_str = content.getText().toString();
+        outState.putString(TITLE_KEY, title_str);
+        outState.putString(CONTENT_KEY, content_str);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String title_str = savedInstanceState.getString(TITLE_KEY, "");
+        String content_str = savedInstanceState.getString(CONTENT_KEY, "");
+        title.setText(title_str);
+        content.setText(content_str);
     }
 }
